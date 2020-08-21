a3dea5031dcc6b3aab959ee5fa6c4c92b58b5b3d
Andre Said <andresaid123@hotmail.com>
2020-08-21 16:53:45 -0300

    Aula 1 e Aula 2 do curso

---
+++ src/main/java/com/algaworks/osworks/OsworksSpringApiApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.osworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsworksSpringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsworksSpringApiApplication.class, args);
+	}
+
+}
